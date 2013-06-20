3028b5e95b6d536f8402edb65a4028b66a34dea3
b18293 <b18293@freescale.com>
2013-06-21 04:43:10 +0800

    fsl_imx_omx.git: generate fsl omx patch

---
+++ OpenMAXIL/src/component/camera_source/Android.mk
@@ -0,0 +1,34 @@
+ifeq ($(HAVE_FSL_IMX_CODEC),true)
+ifneq ($(PREBUILT_FSL_IMX_OMX),true)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+ifeq ($(findstring x4.2,x$(PLATFORM_VERSION)), x4.2)
+LOCAL_SRC_FILES := \
+	CameraSourceJB.cpp
+else
+LOCAL_SRC_FILES := \
+	CameraSource.cpp
+endif
+	
+LOCAL_CFLAGS += $(FSL_OMX_CFLAGS)
+
+LOCAL_LDFLAGS += $(FSL_OMX_LDFLAGS)
+ 
+LOCAL_C_INCLUDES += $(FSL_OMX_INCLUDES) 
+
+LOCAL_SHARED_LIBRARIES := lib_omx_common_v2_arm11_elinux \
+    			  lib_omx_osal_v2_arm11_elinux \
+                          lib_omx_utils_v2_arm11_elinux \
+			  lib_omx_res_mgr_v2_arm11_elinux \
+			  libmedia libutils libmediaplayerservice libcamera_client libbinder
+
+LOCAL_PRELINK_MODULE := false
+	
+LOCAL_MODULE:= lib_omx_camera_source_arm11_elinux
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+endif
